2d6e28b38d3ff495f2134b2b4b09cecb7264fcde
Casper Johansson <caspthepro@gmail.com>
2024-04-27 14:21:41 +0200

    Added tests for ArticleCatalogHandler

--- src/test/java/integration/ArticleCatalogHandlerTest.java
+++ src/test/java/integration/ArticleCatalogHandlerTest.java
@@ -33,11 +33,41 @@ public class ArticleCatalogHandlerTest {
     }
 
     @Test
-    public void testSetSaleTime() {
+    public void testFetchArticleDTOisValid() {
         ArticleDTO expectedOutput = new ArticleDTO(101);
-        // ArticleDTO actualOutput = instanceToTest.fetchArticleDTO(101);
-        ArticleDTO actualOutput = new ArticleDTO(101);
 
-        assertTrue(actualOutput.equals(expectedOutput));
-    }    
+        ArticleDTO actualOutput = instanceToTest.fetchArticleDTO(101);
+
+        boolean identifierMatch = actualOutput.getIdentifier() == expectedOutput.getIdentifier();
+        boolean priceMatch = actualOutput.getPrice() == expectedOutput.getPrice();
+        boolean nameMatch = actualOutput.getName() == expectedOutput.getName();
+        boolean vatRateMatch = actualOutput.getVatRate() == expectedOutput.getVatRate();
+
+        boolean allMatch = identifierMatch && priceMatch && nameMatch && vatRateMatch;
+        assertTrue(allMatch, "The two ArticleDTO objects do not match.");
+    }
+
+    @Test
+    public void testFetchArticleDTODifferentIdentifier() {
+        ArticleDTO expectedOutput = new ArticleDTO(105);
+
+        ArticleDTO actualOutput = instanceToTest.fetchArticleDTO(104);
+
+        boolean identifierMatch = actualOutput.getIdentifier() == expectedOutput.getIdentifier();
+        boolean priceMatch = actualOutput.getPrice() == expectedOutput.getPrice();
+        boolean nameMatch = actualOutput.getName() == expectedOutput.getName();
+        boolean vatRateMatch = actualOutput.getVatRate() == expectedOutput.getVatRate();
+
+        boolean allMatch = identifierMatch && priceMatch && nameMatch && vatRateMatch;
+        assertFalse(allMatch, "The two ArticleDTO objects match.");
+    }
+
+    @Test
+    public void testFetchArticleDTOisNotValid() {
+        ArticleDTO expectedOutput = null;
+
+        ArticleDTO actualOutput = instanceToTest.fetchArticleDTO(99);
+
+        assertTrue(actualOutput == expectedOutput, "The method did not return null with an invalid identifier.");
+    }
 }
