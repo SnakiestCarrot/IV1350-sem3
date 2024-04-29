3b8bb13d882d398f97ee3f825bf69064892250e1
Casper Johansson <caspthepro@gmail.com>
2024-04-29 14:08:17 +0200

    Added article descriptions

--- src/test/java/se/kth/iv1350/amazingpos/model/SaleTest.java
+++ src/test/java/se/kth/iv1350/amazingpos/model/SaleTest.java
@@ -25,13 +25,19 @@ public class SaleTest {
 
     @Test
     public void testEnterArticleToSale() {
-        ArticleDTO testArticleDTO = new ArticleDTO(101, 2.99, 0.25, "Banana");
+        ArticleDTO testArticleDTO = new ArticleDTO(101, 2.99, 0.25, "Banana", "This is a banana");
         Article testArticle = new Article(testArticleDTO, 1);
 
         instanceToTest.enterArticleToSale(testArticleDTO, 1);
 
-        assertTrue(instanceToTest.getArticleList().get(0).equals(testArticle), "The two ArticleDTO objects do not match.");
+        assertTrue(instanceToTest.getArticleList().get(0).equals(testArticleDTO), "The two ArticleDTO objects do not match.");
     }
 
-    
+    @Test
+    public void testEqualsIsFalse() {
+        
+        ArticleDTO expectedOutput = new ArticleDTO(102, 1.99, 0.25, "Apple", "This is an apple");
+
+        assertFalse(instanceToTest.equals(expectedOutput), "The two ArticleDTO objects match.");
+    }
 }
