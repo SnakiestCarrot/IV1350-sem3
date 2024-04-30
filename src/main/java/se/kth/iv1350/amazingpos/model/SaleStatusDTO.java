216fc5677e0a9102ffd186ee10540d52cadce1dd
Filip Boive <filip.boive@gmail.com>
2024-04-30 14:06:12 +0200

    added comments

--- src/main/java/se/kth/iv1350/amazingpos/model/SaleStatusDTO.java
+++ src/main/java/se/kth/iv1350/amazingpos/model/SaleStatusDTO.java
@@ -5,6 +5,14 @@ public class SaleStatusDTO {
     private double currentTotalCost;
     private double currentTotalVAT;
 
+    /**
+     * SaleStatusDTO stores information about the sale, 
+     * like most recently added article, the cost of the sale so far, and the vat of the sale so far.
+     * @param currentArticle the most recently added article
+     * @param currentTotalCost the running total of the sale
+     * @param currentTotalVAT the running total VAT cost of the sale
+     */
+
     SaleStatusDTO (Article currentArticle, double currentTotalCost, double currentTotalVAT) {
         this.currentArticle = currentArticle;
         this.currentTotalCost = currentTotalCost;
